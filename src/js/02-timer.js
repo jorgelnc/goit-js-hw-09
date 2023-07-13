5f752525996865b29a8098f52f2da87e2bd9dafa
jorgelnc <jorgenavacolina@gmail.com>
2023-07-13 14:17:44 -0500

    task 2 added

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,15 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from "notiflix";
 
 const dateTime = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector("button");
+const daysOutput = document.querySelectorAll(".value")[0];
+const hoursOutput = document.querySelectorAll(".value")[1];
+const minutesOutput = document.querySelectorAll(".value")[2];
+const secondsOutput = document.querySelectorAll(".value")[3];
+startBtn.disabled = true;
+let dateNow = Date.now();
 
 const options = {
   enableTime: true,
@@ -9,35 +17,52 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    selectedDate = selectedDates[0];
+    if (selectedDate > dateNow) {
+    startBtn.disabled = false;
+    } else {
+    Notiflix.Notify.warning("Please choose a date in the future");
+    }
+    
   },
 };
 
 flatpickr(dateTime, options);
 
+startBtn.addEventListener("click", onBtn);
 
-// let temporizador;
-// let tiempoInicial;
-// let ejecutando;
+function onBtn() {
+  const timer = setInterval(() => {
+    dateNow = new Date();
 
-// function iniciarTemporizador() {
-//     var fechaInicial = new Date;
-//     fechaInicial.setFullYear(fechaInicial.getFullYear() + 1);
-//     fechaInicial.setMonth(0);
-//     fechaInicial.setDate(1);
-//     fechaInicial.setHours(0);
-//     fechaInicial.setMinutes(0);
-//     fechaInicial.setSeconds(0);
-//     temporizador = setInterval(() => {
-//         actualizarTemporizador(fechaInicial);
-//     })
-// }
+    if (dateNow < selectedDate) {
+      countTimer = convertMs(selectedDate - dateNow);
+      updateTime(countTimer);
+      startBtn.disabled = true;
+    } else {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
 
-// function actualizarTemporizador(fechaFinal) {
-//     let tiempoActual = Date.now();
-//     let tiempoRestante = fechaFinal - tiempoActual;
-//     let segundos = Math.floor(tiempoRestante / 1000) % 60;
-//     let minutos = Math.floor(tiempoRestante / (1000 * 60)) % 60;
-//     let horas = Math.floor(tiempoRestante / (1000 * 60 * 60) % 24);
-//     let dias = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
-// }
+function updateTime(countTimer) {
+  daysOutput.textContent = `${dataOutput.days}`;
+  hoursOutput.textContent = `${dataOutput.hours}`;
+  minutesOutput.textContent = `${dataOutput.minutes}`;
+  secondsOutput.textContent = `${dataOutput.seconds}`;
+}
+
+function convertMs(ms) {
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  dataOutput = { days, hours, minutes, seconds };
+  return dataOutput;
+}
